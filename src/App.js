2de20ce95fec20de805560e7cffe2a32029f4224
Agustin Cordero <43405008+githubAgustinCordero@users.noreply.github.com>
2023-01-05 09:38:32 +0000

    ca,bio1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
+        </p><p>En React la página index.js es la que renderiza todo el contenido html que hay en cada componente.
         </p>
+        <p>Cada componente es una funcion que retorna código html</p>
+        <p>Tambien existe el estado y las propiedades, que se pueden suar importando su libreria en la pagina, como en la pagina de Contador</p>
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
         </p>
